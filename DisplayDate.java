d6bf54f675f7edd219140f36414227b34032dd3a
Akash Ghosh <akashghosh.nyc@gmail.com>
2020-03-15 23:47:44 -0400

    Add files via upload

---
+++ DisplayDate.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class DisplayDate {
+
+    public static void main(String args[]) {
+        // Instantiate a Date object by invoking its constructor
+        Date objDate = new Date();
+        // Display the Date & Time using toString()
+        System.out.println(objDate.toString());
+    }
+}
